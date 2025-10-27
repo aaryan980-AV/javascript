dcb0d3f08b7f3b8f15264c47138eed7373177b44
aaryan980-AV <aaryanvaishnav2005@gmail.com>
2025-10-27 17:00:44 +0000

    variables in js

---
+++ basics/variables.js
@@ -0,0 +1,14 @@
+const accountId = 12233
+let accountEmail = "aaryan@gmail.com"
+var accountPassword = "123456"
+accountCity = "Mumbai"
+let accountStates;
+
+// accountId = 2
+accountEmail = "av@gmail.com"
+accountPassword = "213465" 
+accountCity = "solapur"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountStates])
+
